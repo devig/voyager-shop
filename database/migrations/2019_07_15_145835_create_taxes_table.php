7c0c3c610025b18673a1b203a9ecc45235903e69
Thomas Venturini <thomas@venturini.codes>
2019-07-15 18:56:22 +0200

    add taxes migration

---
+++ database/migrations/2019_07_15_145835_create_taxes_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function () {
+            Schema::create('products', function (Blueprint $table) {
+                $table->bigIncrements('id');
+
+                $table->string('name');
+                $table->integer('tax');
+
+                // create project_id
+                $table->bigInteger('project_id')
+                    ->unsigned()
+                    ->nullable();
+                // make project_id column a foreign key
+                $table->foreign('project_id')
+                    ->references('id')
+                    ->on('projects')
+                    ->onDelete('cascade');
+
+                // create country_id
+                $table->bigInteger('country_id')
+                    ->unsigned()
+                    ->nullable();
+                // make country_id column a foreign key
+                $table->foreign('country_id')
+                    ->references('id')
+                    ->on('countries')
+                    ->onDelete('cascade');
+
+                $table->timestamps();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::transaction(function () {
+            Schema::dropIfExists('products');
+        });
+    }
+}
