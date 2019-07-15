3e5faa87ebfc21c30d2674c0233d2184e0df7565
Thomas Venturini <thomas@venturini.codes>
2019-07-15 18:56:08 +0200

    add countries table

---
+++ database/migrations/2019_07_15_145832_create_countries_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function () {
+            Schema::create('countries', function (Blueprint $table) {
+                $table->bigIncrements('id');
+
+                $table->string('name');
+                $table->string('code');
+
+                // create project_id
+                $table->bigInteger('project_id')
+                    ->unsigned()
+                    ->nullable();
+                // make project_id column a foreign key
+                $table->foreign('project_id')
+                    ->references('id')
+                    ->on('projects')
+                    ->onDelete('cascade');
+
+                $table->timestamps();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::transaction(function () {
+            Schema::dropIfExists('countries');
+        });
+    }
+}
