6a23279e27258cb757f930e40a5f0e375a7a5062
Thomas Venturini <thomas@venturini.codes>
2019-07-15 16:57:50 +0200

    preparing repository

---
+++ resources/lang/en/shop.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Voyager Shop Translations
+|--------------------------------------------------------------------------
+|
+| In this translationfile you will find all translations of this package.
+|
+*/
+
+return [
+    'label' => 'Shop',
+];
