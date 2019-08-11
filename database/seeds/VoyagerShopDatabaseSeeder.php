07a40222c7ac648c1416b23260a99b03ee82e422
Thomas Venturini <thomas@venturini.codes>
2019-08-11 19:18:05 +0200

    add database seeder for install script

---
+++ database/seeds/VoyagerShopDatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tjventurini\VoyagerShop\Seeds;
+
+use Illuminate\Database\Seeder;
+
+class VoyagerShopDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the voyager tags package database seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
