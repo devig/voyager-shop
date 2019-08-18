66128ead1c4d98ac00c6aa0a544e2b22b9b32b54
Thomas Venturini <thomas@venturini.codes>
2019-08-18 16:35:52 +0200

    working on countries data rows and admin panel

--- database/seeds/VoyagerShopDataRowsSeeder.php
+++ database/seeds/VoyagerShopDataRowsSeeder.php
@@ -19,6 +19,143 @@ class VoyagerShopDataRowsSeeder extends Seeder
         $this->orders();
         $this->products();
         $this->productVariants();
+        $this->countries();
+    }
+
+    private function countries()
+    {
+        DB::transaction(function () {
+            // get the data type
+            $data_type = DataType::where('slug', 'countries')->firstOrFail();
+
+            // field id
+            $field_id = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'id',
+            ], [
+                'type' => 'hidden',
+                'display_name' => trans('shop::countries.data_rows.id'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 0,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 0,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+
+            // field name
+            $field_name = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'name',
+            ], [
+                'type' => 'text',
+                'display_name' => trans('shop::countries.data_rows.name'),
+                'required' => 1,
+                'browse' => 1,
+                'read' => 1,
+                'edit' => 1,
+                'add' => 1,
+                'delete' => 1,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+
+            // field code
+            $field_code = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'code',
+            ], [
+                'type' => 'text',
+                'display_name' => trans('shop::countries.data_rows.code'),
+                'required' => 1,
+                'browse' => 1,
+                'read' => 1,
+                'edit' => 1,
+                'add' => 1,
+                'delete' => 1,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+
+            // field project_id
+            $field_project_id = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'project_id',
+            ], [
+                'type' => 'hidden',
+                'display_name' => 'Hidden project id',
+                'required' => 1,
+                'browse' => 1,
+                'read' => 1,
+                'edit' => 1,
+                'add' => 1,
+                'delete' => 1,
+                'details' => [],
+                'order' => 1,
+            ]);
+
+            // field project
+            $field_project = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'product_belongsto_project_relationship',
+            ], [
+                'type' => 'relationship',
+                'display_name' => 'Project',
+                'required' => 1,
+                'browse' => 1,
+                'read' => 1,
+                'edit' => 1,
+                'add' => 1,
+                'delete' => 1,
+                'details' => [
+                    'model' => "Tjventurini\\VoyagerProjects\\Models\\Project",
+                    'table' => 'projects',
+                    'type' => 'belongsTo',
+                    'column' => 'project_id',
+                    'key' => 'id',
+                    'label' => 'name',
+                    'pivot' => 0,
+                    'taggable' => 0,
+                ],
+                'order' => 1,
+            ]);
+
+            // field created_at
+            $field_created_at = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'created_at',
+            ], [
+                'type' => 'timestamp',
+                'display_name' => trans('shop::countries.data_rows.created_at'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 1,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 1,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+
+            // field updated_at
+            $field_updated_at = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'updated_at',
+            ], [
+                'type' => 'timestamp',
+                'display_name' => trans('shop::countries.data_rows.updated_at'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 1,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 1,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+        });
     }
 
     private function productVariants()
@@ -138,6 +275,40 @@ class VoyagerShopDataRowsSeeder extends Seeder
                     'pivot' => 0,
                     'taggable' => 0,
                 ],
+                'order' => 1,
+            ]);
+
+            // field created_at
+            $field_created_at = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'created_at',
+            ], [
+                'type' => 'timestamp',
+                'display_name' => trans('shop::product-variants.data_rows.created_at'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 1,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 1,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+
+            // field updated_at
+            $field_updated_at = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'updated_at',
+            ], [
+                'type' => 'timestamp',
+                'display_name' => trans('shop::product-variants.data_rows.updated_at'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 1,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 1,
+                'details' => json_encode([]),
                 'order' => 1,
             ]);
         });
@@ -319,6 +490,40 @@ class VoyagerShopDataRowsSeeder extends Seeder
                     'pivot' => 0,
                     'taggable' => 0,
                 ],
+                'order' => 1,
+            ]);
+
+            // field created_at
+            $field_created_at = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'created_at',
+            ], [
+                'type' => 'timestamp',
+                'display_name' => trans('shop::products.data_rows.created_at'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 1,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 1,
+                'details' => json_encode([]),
+                'order' => 1,
+            ]);
+
+            // field updated_at
+            $field_updated_at = DataRow::updateOrCreate([
+                'data_type_id' => $data_type->id,
+                'field' => 'updated_at',
+            ], [
+                'type' => 'timestamp',
+                'display_name' => trans('shop::products.data_rows.updated_at'),
+                'required' => 1,
+                'browse' => 0,
+                'read' => 1,
+                'edit' => 0,
+                'add' => 0,
+                'delete' => 1,
+                'details' => json_encode([]),
                 'order' => 1,
             ]);
         });
--- resources/lang/en/countries.php
+++ resources/lang/en/countries.php
@@ -13,4 +13,12 @@
 return [
     'label_singular' => 'Country',
     'label_plural' => 'Countries',
+
+    'data_rows' => [
+        'id' => 'ID',
+        'name' => 'Name',
+        'code' => 'Code',
+        'created_at' => 'Created at',
+        'updated_at' => 'Updated at',
+    ],
 ];
